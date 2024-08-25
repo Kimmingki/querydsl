895a0d9251e726137bada99c0a77a20d7246f35e
Kimmingki <rlaalsrl9569@naver.com>
2024-08-25 10:56:40 +0900

    동적 쿼리와 성능 최적화 조회 - Builder 사용

--- src/test/java/study/querydsl/repository/MemberJPARepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJPARepositoryTest.java
@@ -1,11 +1,16 @@
 package study.querydsl.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDTO;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 
@@ -20,6 +25,23 @@ class MemberJPARepositoryTest {
     
     @Autowired
     MemberJPARepository memberJPARepository;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
     
     @Test
     public void basicTest() throws Exception {
@@ -34,5 +56,17 @@ class MemberJPARepositoryTest {
 
         List<Member> result2 = memberJPARepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(35);
+//        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDTO> result = memberJPARepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
 }
